e1bac5f6fc569f3a9f14ec8a967854695cee99a1
Tirthankar Pal <141252995+Tirthankarpal@users.noreply.github.com>
2025-12-06 13:59:55 +0530

    Add Little Fairy's Painting [A] from Codeforces

---
+++ 2175/A - Little Fairy's Painting/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;typedef long long ll;int main(){    int t;    cin >> t;    while(t--){        ll n;        cin>>n;        set<ll> st;        for(int i=0;i<n;i++){            ll x;            cin>>x;            st.insert(x);        }        ll len=st.size();        ll ans=*st.lower_bound(len);        cout<<ans<<endl;    }}
